af6cd74998eaa05e69a88a9a3f9045696a19d495
AI 공부 <103333989+Josoobeen@users.noreply.github.com>
2024-03-21 11:47:02 +0900

    Update sublayer_connection.py

--- model_parts/sublayer_connection.py
+++ model_parts/sublayer_connection.py
@@ -1,1 +1,14 @@
+import tensorflow as tf
+from keras.layers import Dropout
 
+def sublayer_connection(inputs, sublayer, dropout = 0.2):
+    inputs = inputs + Dropout(dropout)(sublayer)
+    
+    feature_shape = inputs.get_shape()[-1:]
+    mean = tf.keras.backend.mean(inputs, [-1], keepdims = True)
+    std = tf.keras.backend.std(inputs, [-1], keepdims = True)
+    beta = tf.cast(tf.zeros(feature_shape), tf.float32)
+    gamma = tf.cast(tf.ones(feature_shape), tf.float32)
+    
+    layer_norm = gamma *(inputs - mean) / (std + 1e-6) + beta
+    return layer_norm
